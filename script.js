cc08650f4876b60ab9b607e418fa1dd64f79397a
Shubham Singh <122176279+singhsubm@users.noreply.github.com>
2023-08-06 17:22:51 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,4 @@
+var scroll = new SmoothScroll('a[href="#"]', {
+    speed: 200, // Set the scrolling speed in milliseconds
+    easing: 'easeIn' // Set the easing function for smooth scroll animation
+});
